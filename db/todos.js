416221ac50305b69a924ee82d316e0251b8bebfa
bluuweb <contacto@bluuweb.cl>
2024-04-11 11:55:04 -0400

    first commit

---
+++ db/todos.js
@@ -0,0 +1,12 @@
+// cómo se llama esta exportación?
+// RE: exportaciones nombradas
+export const todos = [
+    { id: 1, title: 'Estudiar CSS', done: false },
+    { id: 2, title: 'Estudiar Node.js', done: false },
+    { id: 3, title: 'Estudiar Jquery', done: true },
+];
+
+export const name = "juanito"
+
+// export por defecto
+export default name;
