acd9a7efda60bbb5251bb3dd747ba6b638a9a66d
bluuweb <contacto@bluuweb.cl>
2024-04-11 13:59:05 -0400

    json

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 import express from 'express'
+import { writeFile, readFile } from 'node:fs/promises'
 
 const app = express()
+app.get('/api/create-todos', async (req, res) => {
 
-// estoy escuchando si hacen la petición a: http://localhost:5000/api/v2/pokemon/ditto
-app.post('/api/v2/pokemon/ditto', (req, res) => {
+    // array de objetos !== json de array de objetos
+    const todos = [
+        { id: 1, title: 'Estudiar CSS', done: false },
+        { id: 2, title: 'Estudiar Node.js', done: false },
+        { id: 3, title: 'Estudiar Jquery', done: true },
+    ];
+
+    // Que tipo de dato es un json? string, objeto, array, number??
+
+    await writeFile('./todos.json', JSON.stringify(todos))
     res.json({
-        name: 'ditto',
-        id: '131'
+        ok: true,
+        msg: "archivo creado"
+    })
+})
+
+app.get('/api/todos', async (req, res) => {
+    const stringJSON = await readFile("./todos.json", "utf-8")
+    res.json({
+        ok: true,
+        msg: stringJSON
     })
 })
 
