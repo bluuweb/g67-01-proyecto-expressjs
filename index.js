db32392f2bcba63cab3f658ae9a1ff30b1b8311d
bluuweb <contacto@bluuweb.cl>
2024-04-11 16:36:07 -0400

    todos

--- index.js
+++ index.js
@@ -3,15 +3,38 @@ import { engine } from 'express-handlebars';
 
 const app = express()
 
+const todos = [
+    { id: 1, title: 'Estudiar CSS', done: false },
+    { id: 2, title: 'Estudiar Node.js', done: false },
+    { id: 3, title: 'Estudiar Jquery', done: true },
+];
+
 // Motor de plantillas handlebars
 app.engine('.hbs', engine({ extname: '.hbs' }));
 app.set('view engine', '.hbs');
 app.set('views', './views');
 
+// todos
+app.get('/todos', (req, res) => {
+    res.render('todos', { todos });
+});
+
 // GET http://localhost:5000/
 app.get('/', (req, res) => {
     res.render('home');
 });
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+app.get('/contact', (req, res) => {
+    res.render('contact');
+});
+
+
+
+// crear rutas: about y contact
 
 app.listen(5000, () => {
     console.log('estoy escuchando peticiones')
